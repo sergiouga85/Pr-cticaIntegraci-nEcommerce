0c58e9914d43dc89119619ba02a345dcef1396ee
Sergio Ugarte <sergiouga85@gmail.com>
2023-11-30 19:31:07 -0300

    segundo commit practica integradora

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 import { MONGODB_CNX_STR } from '../src/config.js'
 import { productsManager } from '../dao/ProductsManager.mongoose.js'
-import { cartsManagerMongodb } from '../dao/CartsManager.mongoose.js'
+import { cartsManager } from '../dao/CartsManager.mongoose.js'
+import { messagesManager } from '../dao/MessagesManager.mongoose.js'
 
 
 
@@ -25,9 +26,22 @@ console.log(await productsManager.findById("27ba5e92-77cb-401f-ab47-b1ce576d9058
 
 
 
-await cartsManagerMongodb.registrar({
-  /*direccion: 'aca 1234',
-  cantAmbientes: 4,*/
+await cartsManager.create({
+   products: [
+    {
+       id: "add06325-173d-4a7d-8ec6-594b17db8482",
+      quantity: 14
+    }
+  ]
 })
 
-console.log(await propiedadesManager.consultar())
+console.log(await cartsManager.findAll())
+
+await cartsManager.create({ 
+  
+    user: "sergiouga85@gmail.com",
+    message: "Hola, buenas tardes mi nombre es sergio queria consultar sobre un producto"
+  
+})
+
+console.log(await messagesManager.findAll())
